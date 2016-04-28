1b0eef55e7d3ca69b3dfcc0db401be3f5a1b7c7e
Leon Stam <Stoux2@gmail.com>
2016-04-28 13:37:11 +0200

    Model tests

--- src/test/java/nl/lijstr/domain/base/IdCmModelTest.java
+++ src/test/java/nl/lijstr/domain/base/IdCmModelTest.java
@@ -1,8 +1,10 @@
 package nl.lijstr.domain.base;
 
+import java.time.LocalDateTime;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static nl.lijstr._TestUtils.TestUtils.*;
 import static org.junit.Assert.*;
@@ -10,19 +12,59 @@ import static org.mockito.Mockito.*;
 
 /**
  * Created by Stoux on 29/01/2016.
- * TODO
  */
-@Ignore
 public class IdCmModelTest {
 
-    @Before
-    public void setUp() throws Exception {
+    @Test
+    public void createdFillTimes() {
+        //Arrange
+        TestModel testModel = new TestModel();
 
+        //Pre-act asserts
+        assertNull(testModel.getCreated());
+        assertNull(testModel.getLastModified());
+
+        //Act
+        ReflectionTestUtils.invokeMethod(testModel, "fillTimes");
+        changeTime();
+
+        //Assert
+        assertNotNull(testModel.getCreated());
+        assertEquals(testModel.getCreated(), testModel.getLastModified());
+        assertTrue(testModel.getCreated().isAfter(LocalDateTime.now().minusMinutes(1)));
+        assertTrue(testModel.getCreated().isBefore(LocalDateTime.now()));
     }
 
     @Test
-    public void testFillTimes() {
-        //TODO:
+    public void modifyFillTimes() {
+        //Arrange
+        TestModel testModel = new TestModel();
+        final LocalDateTime dateTime = LocalDateTime.now().minusSeconds(5);
+        testModel.setCreated(dateTime);
+        testModel.setLastModified(dateTime);
+
+        //Act
+        ReflectionTestUtils.invokeMethod(testModel, "fillTimes");
+        changeTime();
+
+        //Assert
+        assertNotNull(testModel.getCreated());
+        assertNotNull(testModel.getLastModified());
+        assertNotEquals(testModel.getCreated(), testModel.getLastModified());
+        assertTrue(testModel.getCreated().isBefore(testModel.getLastModified()));
+        assertTrue(testModel.getLastModified().isBefore(LocalDateTime.now()));
+    }
+
+    private void changeTime() {
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            fail("Failed to sleep...");
+        }
+    }
+
+    private class TestModel extends IdCmModel {
+
     }
 
 }
